0cab6425c096e42264d1ac44190ceca30aedc365
zhimingdai <45969196+zhimingdai@users.noreply.github.com>
2018-12-20 15:59:11 +0800

    Update 04_pull_kubernetes_node_images_from_aliyun.sh

--- kubeadmv1.11.0/04_pull_kubernetes_node_images_from_aliyun.sh
+++ kubeadmv1.11.0/04_pull_kubernetes_node_images_from_aliyun.sh
@@ -5,11 +5,13 @@ set -e
 # Check version in https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-init/
 # Search "Running kubeadm without an internet connection"
 # For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
+#Use domestic resources to download the image
 KUBE_VERSION=v1.11.0
 KUBE_PAUSE_VERSION=3.1
 
 GCR_URL=k8s.gcr.io
-ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
+ALIYUN_URL=registry.cn-shanghai.aliyuncs.com/daizhiming
+#registry.cn-shanghai.aliyuncs.com/daizhiming
 
 # When test v1.11.0, I found Kubernetes depends on both pause-amd64:3.1 and pause:3.1 
 
@@ -20,6 +22,7 @@ pause:${KUBE_PAUSE_VERSION})
 
 for imageName in ${images[@]} ; do
   docker pull $ALIYUN_URL/$imageName
+  #Rename the image using the "docker tag" command
   docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
   docker rmi $ALIYUN_URL/$imageName
 done
