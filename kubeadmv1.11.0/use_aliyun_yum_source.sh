e5db251e4f0cb035a956f9d1e3f01db7a4b5c692
zhimingdai <45969196+zhimingdai@users.noreply.github.com>
2018-12-20 14:59:44 +0800

    Update use_aliyun_yum_source.sh

--- kubeadmv1.11.0/use_aliyun_yum_source.sh
+++ kubeadmv1.11.0/use_aliyun_yum_source.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#centos7配置使用阿里云的yum源
+
 set -e
 
+#直接把文件放在当前目录里面，也可以执行下一行的命令从网上下载
 #wget -O /etc/yum.repos.d/CentOS7-Aliyun.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 cp -p ./CentOS7-Aliyun.repo /etc/yum.repos.d
 yum clean all
